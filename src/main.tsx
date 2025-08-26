c1066f6a400119af755c6735c93a5893a0ee3ea6
applepolish503 <applepolish503@gmail.com>
2025-08-26 21:40:16 +0900

    fix 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { App } from "./routes/App";
 import { Login } from "./routes/Login";
 import { Main } from "./routes/Main";
@@ -10,14 +10,14 @@ if (!rootElement) {
   throw new Error("Root element with id 'root' not found");
 }
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <App />,
     children: [
       { index: true, element: <Login /> },
       { path: "main", element: <Main /> },
     ]
   }
-]);
+], { basename: "/" });
 
 createRoot(rootElement).render(
   <React.StrictMode>
